12662cbbfa9926e87fbe3564540d67b95560056b
OlgaKaa <kasl88@bk.ru>
2018-07-01 16:13:33 +0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,24 @@ window.addEventListener('load', function(e){
     var inp2 = document.querySelector('.inp2');
     var btn = document.querySelector('.go');
     var res = document.querySelector('.res');
+    var sel = document.querySelector('select');
     
     btn.addEventListener('click', function(){
-    	var sum = parseFloat(inp1.value) + parseFloat(inp2.value);
+    	var operation = sel.options[sel.selectedIndex].innerHTML;
+    	var sum = eval(parseFloat(inp1.value) + operation + parseFloat(inp2.value));
     	res.innerHTML = sum;
     	btn.disabled = true;
     });
+
+    inp1.addEventListener('keypress', undisable);
+    inp2.addEventListener('keypress', undisable);
+
+    function undisable(event) {
+       btn.disabled = false;
+       if (event.keyCode < 48 || event.keyCode > 57) {
+        event.preventDefault();
+    }
+    }
 
 });
 
